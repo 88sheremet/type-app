d4fc3ef0e5ab56d54fae18012a5049091819bb61
Oleksandr Sheremet <88sheremet@gmail.com>
2023-04-22 21:41:16 +0300

    add Home component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
-    <h1>Home Page</h1>
+    <Home></Home>
     </div>
   );
 }
